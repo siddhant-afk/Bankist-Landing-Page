0c10b19ec5fbf60e00a5d056646f9e48adcfebfb
siddhant-afk <siddhantdudhane21@gmail.com>
2024-11-01 12:26:03 +0530

    Implement Tab Component

--- script.js
+++ script.js
@@ -81,3 +81,26 @@ document
       document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
     }
   });
+
+// Tabbed component
+const tabs = document.querySelectorAll('.operations__tab');
+
+const tabsContainer = document.querySelector('.operations__tab-container');
+const tabsContent = document.querySelectorAll('.operations__content');
+
+tabsContainer.addEventListener('click', function (event) {
+  const clicked = event.target.closest('.operations__tab');
+
+  if (!clicked) return;
+
+  // Remove active classes
+  tabs.forEach(tab => tab.classList.remove('operations__tab--active'));
+  tabsContent.forEach(c => c.classList.remove('operations__content--active'));
+
+  // Adding active classes
+  clicked.classList.add('operations__tab--active');
+
+  document
+    .querySelector(`.operations__content--${clicked.dataset.tab}`)
+    .classList.add('operations__content--active');
+});

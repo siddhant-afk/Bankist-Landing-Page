81f3c7b04856717c6197af204942f1bde49c3f6d
siddhant-afk <siddhantdudhane21@gmail.com>
2024-11-04 15:55:30 +0530

    Implement Sticky Navigation using the Intersection Observer API

--- script.js
+++ script.js
@@ -126,3 +126,21 @@ const handleHover = function (e) {
 
 nav.addEventListener('mouseover', handleHover.bind(0.5));
 nav.addEventListener('mouseout', handleHover.bind(1));
+
+// Implement Sticky Navigation
+const stickyNav = function (entries) {
+  const [entry] = entries;
+
+  if (!entry.isIntersecting) nav.classList.add('sticky');
+  else nav.classList.remove('sticky');
+};
+
+const header = document.querySelector('.header');
+const navHeight = nav.getBoundingClientRect().height;
+const headerObserver = new IntersectionObserver(stickyNav, {
+  root: null,
+  threshold: 0,
+  rootMargin: `-${navHeight}px`,
+});
+
+headerObserver.observe(header);
